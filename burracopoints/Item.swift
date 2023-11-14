538a345a33baecf826912420370ae59a2ac9dff4
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-11-14 10:43:31 +0100

    Initial Commit

---
+++ burracopoints/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  burracopoints
+//
+//  Created by Matteo Perotta on 14/11/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
