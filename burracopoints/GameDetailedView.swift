bd9614127fef9ed08820d7491c4c28a5a523c833
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-11-17 10:31:48 +0100

    -.

--- burracopoints/GameDetailedView.swift
+++ burracopoints/GameDetailedView.swift
@@ -8,20 +8,110 @@
 import SwiftUI
 
 struct GameAddPointsSheetView: View {
+    
     @Bindable var displayedGame: Game
+    
+    @State private var squad1pointsscore: String = ""
+    @State private var squad1pointsbase: String = ""
+    @State private var squad2pointsscore: String = ""
+    @State private var squad2pointsbase: String = ""
+    @State private var squad3pointsscore: String = ""
+    @State private var squad3pointsbase: String = ""
+    
     @Environment(\.dismiss) var dismiss
+    
     var body: some View {
         NavigationView{
             VStack{
                //We need at least 2 input boxes, 3 maximum
                 if displayedGame.gameMode == 2 { 
                     // 1 vs 1
+                    HStack{
+                        VStack{
+                            //player 1 points
+                            GroupBox{
+                                Text(displayedGame.squad1.first!)
+                                TextField("Base", text: $squad1pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad1pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                        
+                        VStack{
+                            GroupBox{
+                                //player 2 points
+                                Text(displayedGame.squad2.first!)
+                                TextField("Base", text: $squad2pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad2pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                    }
                     
                 } else if displayedGame.gameMode == 3 {
                     // 1 vs 1 vs 1, 3 boxes
+                    HStack{
+                        VStack{
+                            //player 1 points
+                            GroupBox{
+                                Text(displayedGame.squad1.first!)
+                                TextField("Base", text: $squad1pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad1pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                        
+                        VStack{
+                            GroupBox{
+                                //player 2 points
+                                Text(displayedGame.squad2.first!)
+                                TextField("Base", text: $squad2pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad2pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                        
+                        VStack{
+                            GroupBox{
+                                //player 2 points
+                                Text(displayedGame.squad3.first!)
+                                TextField("Base", text: $squad3pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad3pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                    }
                     
                 } else if displayedGame.gameMode == 4 {
                     //2 vs 2, 2x2 boxes
+                    HStack{
+                        VStack{
+                            //player 1 points
+                            GroupBox{
+                                Text(displayedGame.squad1.first! + " & " + displayedGame.squad1[1])
+                                TextField("Base", text: $squad1pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad1pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                        
+                        VStack{
+                            GroupBox{
+                                //player 2 points
+                                Text(displayedGame.squad2.first! + " & " + displayedGame.squad2[1])
+                                TextField("Base", text: $squad2pointsbase)
+                                    .padding().keyboardType(.decimalPad)
+                                TextField("Score", text: $squad2pointsscore)
+                                    .padding().keyboardType(.decimalPad)
+                            }
+                        }.padding()
+                    }
                 }
                     
                 
@@ -32,11 +122,42 @@ struct GameAddPointsSheetView: View {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Done") {
                         //Add points and then dismiss
-                        dismiss()
+                        //TODO: check the input
+                        if displayedGame.squad3Enabled {
+                            displayedGame.currentPoints_p1 += Int(squad1pointsbase)! + Int(squad1pointsscore)!
+                            displayedGame.currentPoints_p2 += Int(squad2pointsbase)! + Int(squad2pointsscore)!
+                            displayedGame.currentPoints_p3 += Int(squad3pointsbase)! + Int(squad3pointsscore)!
+                            
+                            var currentHand = displayedGame.handPoints_p1.count
+                            
+                            displayedGame.handPoints_p1[currentHand] = Int(squad1pointsbase)! + Int(squad1pointsscore)!
+                            displayedGame.handPoints_p2[currentHand] = Int(squad2pointsbase)! + Int(squad2pointsscore)!
+                            displayedGame.handPoints_p3[currentHand] = Int(squad3pointsbase)! + Int(squad3pointsscore)!
+                            dismiss()
+                        } else {
+                            print(squad1pointsbase + "  -  " + squad2pointsbase)
+                            print(squad1pointsscore + "  -  " + squad2pointsscore)
+                            
+                            displayedGame.currentPoints_p1 += Int(squad1pointsbase)! + Int(squad1pointsscore)!
+                            displayedGame.currentPoints_p2 += Int(squad2pointsbase)! + Int(squad2pointsscore)!
+                            
+                            var currentHand = displayedGame.handPoints_p1.count
+                            
+                           
+                            displayedGame.handPoints_p1.append( Int( Int(squad1pointsbase)! + Int(squad1pointsscore)! ))
+                            displayedGame.handPoints_p2.append( Int( Int(squad2pointsbase)! + Int(squad2pointsscore)! ))
+                            dismiss()
+                        }
                     }
                 }
                 ToolbarItem(placement: .cancellationAction) {
                     Button("Cancel") {
+                        squad1pointsbase = "0"
+                        squad1pointsscore = "0"
+                        squad2pointsbase = "0"
+                        squad2pointsscore = "0"
+                        squad3pointsbase = "0"
+                        squad3pointsscore = "0"
                         dismiss()
                     }
                 }
@@ -46,6 +167,10 @@ struct GameAddPointsSheetView: View {
     }
 }
 
+
+
+
+
 struct GameDetailedView: View {
     @Bindable var displayedGame: Game
     @State var title: String
@@ -53,9 +178,112 @@ struct GameDetailedView: View {
     var body: some View {
         NavigationStack{
             VStack{
-                HStack{
+                if displayedGame.gameMode == 2 {
+                    HStack{
+                        GroupBox{
+                            VStack{
+                                Text(displayedGame.squad1.first!).bold()
+                                HStack{
+                                    Text("Points: ")
+                                    Text(String(displayedGame.currentPoints_p1))
+                                }
+                                
+                                ForEach(0..<displayedGame.handPoints_p1.count, id: \.self){ index in
+                                    HStack{
+                                        Text("Hand " + String(index) + ":")
+                                        Text(String(displayedGame.handPoints_p1[index]))
+                                    }
+                                }
+                            }
+                        }
+                        GroupBox{
+                            VStack{
+                                Text(displayedGame.squad2.first!).bold()
+                                HStack{
+                                    Text("Points: ")
+                                    Text(String(displayedGame.currentPoints_p2))
+                                }
+                                ForEach(0..<displayedGame.handPoints_p2.count, id: \.self){ index in
+                                    HStack{
+                                        Text("Hand " + String(index) + ":")
+                                        Text(String(displayedGame.handPoints_p2[index]))
+                                    }
+                                }
+                            }
+                        }
+                    }
                     
+                   
+                } else if displayedGame.gameMode == 3 {
+                    // 1 vs 1 vs 1, 3 boxes
+                    HStack{
+                        VStack{
+                            //player 1 points
+                            GroupBox{
+                                VStack{
+                                    Text(displayedGame.squad1.first!)
+                                    Text(String(displayedGame.currentPoints_p1))
+                                    ForEach(0..<displayedGame.handPoints_p1.count, id: \.self){ index in
+                                        Text("Hand " + String(index) + ":")
+                                        Text(String(displayedGame.handPoints_p1[index]))
+                                    }
+                                }
+                                
+                            }
+                        }.padding()
+                        
+                        VStack{
+                            GroupBox{
+                                //player 2 points
+                                Text(displayedGame.squad2.first!)
+                                Text(String(displayedGame.currentPoints_p2))
+                                ForEach(0..<displayedGame.handPoints_p2.count, id: \.self){ index in
+                                    Text("Hand " + String(index) + ":")
+                                    Text(String(displayedGame.handPoints_p2[index]))
+                                }
+                            }
+                        }.padding()
+                        
+                        VStack{
+                            GroupBox{
+                                //player 3 points
+                                Text(displayedGame.squad3.first!)
+                                Text(String(displayedGame.currentPoints_p3))
+                                ForEach(0..<displayedGame.handPoints_p3.count, id: \.self){ index in
+                                    Text("Hand " + String(index) + ":")
+                                    Text(String(displayedGame.handPoints_p3[index]))
+                                }
+                            }
+                        }.padding()
+                    }
+                } else if displayedGame.gameMode == 4 {
+                    //2 vs 2, 2x2 boxes
+                    HStack{
+                        VStack{
+                            //player 1 points
+                            GroupBox{
+                                Text(displayedGame.squad1.first! + " & " + displayedGame.squad1[1])
+                                Text(String(displayedGame.currentPoints_p1))
+                                ForEach(0..<displayedGame.handPoints_p1.count, id: \.self){ index in
+                                    Text("Hand " + String(index) + ":")
+                                    Text(String(displayedGame.handPoints_p1[index]))
+                                }
+                            }
+                        }.padding()
+                        VStack{
+                            GroupBox{
+                                //player 2 points
+                                Text(displayedGame.squad2.first! + " & " + displayedGame.squad2[1])
+                                Text(String(displayedGame.currentPoints_p2))
+                                ForEach(0..<displayedGame.handPoints_p2.count, id: \.self){ index in
+                                    Text("Hand " + String(index) + ":")
+                                    Text(String(displayedGame.handPoints_p2[index]))
+                                }
+                            }
+                        }.padding()
+                    }
                 }
+                
                 
             }
         } .toolbar {
