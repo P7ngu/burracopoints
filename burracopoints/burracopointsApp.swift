c743cab9f759b43e1de33ab0d96d2bd68ecfabbb
P7ngu <60442992+P7ngu@users.noreply.github.com>
2024-07-29 11:56:50 +0200

    1.201: better localization.

--- burracopoints/burracopointsApp.swift
+++ burracopoints/burracopointsApp.swift
@@ -14,26 +14,23 @@ import SwiftData
 @main
 struct burracopointsApp: App {
     
-    // HIGH priority
-    
+    //MARK: HIGH priority
     //TODO: manually pick winner, edit button for user
-    
     //TODO: update win ratio accordingly.
-    //  tell the user which one has to be dealer, turn by turn
-    
-    //done: FIX UNDO AFTER WIN.
-    
+    //TODO: tell the user which one has to be dealer, turn by turn
+    //TODO: 155 / 2 = 75 or 80 lol
     
     //Future future updates
     //TODO: widget that shows points for the last game
     
-    //DONE:
+    //MARK: DONE:
     // fix wins ____
     //sort games by time: most recent ones on top, divide them by finished and not
     //pick winner after hand points insertion, set the winner a win, the loser(s) a lost game,
     //after the creation of a game consider opening the game detailed view instead of the games section
     //username costraints
-    //  tell the user which one has to be dealer
+    //tell the user which one has to be dealer
+    //FIX UNDO AFTER WIN.
     
     
     var sharedModelContainer: ModelContainer = {
